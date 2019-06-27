3e253d4a056b6a206c13a3a3c3779093b4f9cd23
Scars and Bullets <sbrough@gmail.com>
2019-06-27 07:56:12 -0600

    starting over

--- models/Article.js
+++ models/Article.js
@@ -7,7 +7,7 @@ var Schema = mongoose.Schema;
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
-  title: {
+  headline: {
     type: String,
     required: true
   },
@@ -16,13 +16,11 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
+  summary: {
+    type: String,
+    required: true
   }
+
 });
 
 // This creates our model from the above schema, using mongoose's model method
