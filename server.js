190bec34d17bcdf7abfbedbea4e0fc373044bc0f
Stewart Brough <sbrough@gmail.comgit config --global user.emailgit config --global user.emailgit configgit config --global user.email sbrough@gmail.com>
2019-07-03 02:59:29 -0600

    fixed Mongoose deprecation warnings

--- server.js
+++ server.js
@@ -19,28 +19,38 @@ require("./config/routes")(router);
 app.use(express.static(__dirname + "/public"));
 
 // Connect Handlebars to the Express app
-app.engine("handlebars", expressHandlebars({
-  defaultLayout: "main"
-}));
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
 // All requests go through middleware
 app.use(router);
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+let MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, error => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Mongoose DB connection is successful")
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true, useCreateIndex: true },
+  error => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Mongoose DB connection is successful");
+    }
   }
-});
+);
 
 // Listen on designated port
 app.listen(PORT, () => {
